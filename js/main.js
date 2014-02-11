212f642be4b6cc4ed37759a4cc59eab99adb10cb
Brian Schroer <schroer.brian@gmail.com>
2014-02-11 01:11:11 -0800

    Implement GET request to access json of emails

--- js/main.js
+++ js/main.js
@@ -17,6 +17,33 @@ app.controller("HomeController", function($scope){
 
 });
 
-app.controller("settingsController", function($scope){
-  $scope.name = "Ari"
+app.controller("MailListingController", function($scope, $http) {
+  $scope.email = [];
+
+  $http({
+    method: 'GET',
+    url: "/api/mail"
+  })
+  .success(function(data, status, headers) {
+    $scope.email = data.all;
+  })
+  .error(function(data, status, headers) {
+
+  });
 });
+
+app.controller("ContentController", function($scope) {
+
+})
+
+app.controller("settingsController", function($scope){
+  $scope.settings = {
+    name: "John Doe",
+    email: "me@example.com"
+  };
+
+  $scope.updateSettings = function() {
+    console.log("updateSettings was called");
+  }
+});
+
